5b63b2877585404ce3bf254e74061392410348fa
Andrew Cameron <andrew@acameron.org>
2025-10-31 11:47:30 -0700

    code quality fixups c/o pylint

--- src/webcam_demo/__init__.py
+++ src/webcam_demo/__init__.py
@@ -1,2 +1,10 @@
+"""This package comprises the backend for a demo project.
+
+The backend demonstrates data modelling and API design, and provides
+implementations of computer vision demos. The visualizations for these
+live in the frontend."""
+
+
 def main() -> None:
+    """Main entry point for the backend."""
     print("Hello from webcam-demo!")
--- src/webcam_demo/bigwhite.py
+++ src/webcam_demo/bigwhite.py
@@ -10,56 +10,66 @@ from PIL.Image import Image, open as open_image
 
 
 _digits = re.compile("[0-9]+")
+_DEFAULT_TIMEOUT_SECS = 5.0
 
 
 class BigWhite:
+    """A data source for webcam imagery from Big White."""
+
     class Webcam(Enum):
         """identifier for a specific webcam at Big White"""
 
-        VillageCentre = auto()
-        PowCam = auto()
-        TheCliff = auto()
-        EasyStreet = auto()
-        HappyValley = auto()
-        Bullet = auto()
-        TubePark = auto()
-        TelusPark = auto()
-        SnowGhost = auto()
-        BlackForest = auto()
-        GemLakeTop = auto()
-        GemLakeBottom = auto()
+        VILLAGE_CENTRE = auto()
+        POW_CAM = auto()
+        THE_CLIFF = auto()
+        EASY_STREET = auto()
+        HAPPY_VALLEY = auto()
+        BULLET = auto()
+        TUBE_PARK = auto()
+        TELUS_PARK = auto()
+        SNOW_GHOST = auto()
+        BLACK_FOREST = auto()
+        GEM_LAKE_TOP = auto()
+        GEM_LAKE_BOTTOM = auto()
 
     class BigWhiteScrapingError(Exception):
         """an error occurred while scraping Big White's website"""
 
     _INDEX_URL: str = "https://www.bigwhite.com/mountain-conditions/webcams"
     _URL_FORMATS: dict[Webcam, str] = {
-        Webcam.VillageCentre: "https://www.bigwhite.com/sites/default/files/village_{}.jpg",
-        Webcam.PowCam: "http://www.bigwhite.com/sites/default/files/powpow_{}.jpg",
-        Webcam.TheCliff: "http://www.bigwhite.com/sites/default/files/cliff_{}.jpg",
-        Webcam.EasyStreet: "http://www.bigwhite.com/sites/default/files/hwy33_{}.jpg",
-        Webcam.HappyValley: "http://www.bigwhite.com/sites/default/files/happyvalley_{}.jpg",
-        Webcam.Bullet: "http://www.bigwhite.com/sites/default/files/bullet_{}.jpg",
-        Webcam.TubePark: "http://www.bigwhite.com/sites/default/files/tubepark_{}.jpg",
-        Webcam.TelusPark: "http://www.bigwhite.com/sites/default/files/teluspark_{}.jpg",
-        Webcam.SnowGhost: "http://www.bigwhite.com/sites/default/files/snowghost_{}.jpg",
-        Webcam.BlackForest: "http://www.bigwhite.com/sites/default/files/blackforest_{}.jpg",
-        Webcam.GemLakeTop: "http://www.bigwhite.com/sites/default/files/gemlake_{}.jpg",
-        Webcam.GemLakeBottom: "http://www.bigwhite.com/sites/default/files/westridge_{}.jpg",
+        Webcam.VILLAGE_CENTRE: "https://www.bigwhite.com/sites/default/files/village_{}.jpg",
+        Webcam.POW_CAM: "http://www.bigwhite.com/sites/default/files/powpow_{}.jpg",
+        Webcam.THE_CLIFF: "http://www.bigwhite.com/sites/default/files/cliff_{}.jpg",
+        Webcam.EASY_STREET: "http://www.bigwhite.com/sites/default/files/hwy33_{}.jpg",
+        Webcam.HAPPY_VALLEY: "http://www.bigwhite.com/sites/default/files/happyvalley_{}.jpg",
+        Webcam.BULLET: "http://www.bigwhite.com/sites/default/files/bullet_{}.jpg",
+        Webcam.TUBE_PARK: "http://www.bigwhite.com/sites/default/files/tubepark_{}.jpg",
+        Webcam.TELUS_PARK: "http://www.bigwhite.com/sites/default/files/teluspark_{}.jpg",
+        Webcam.SNOW_GHOST: "http://www.bigwhite.com/sites/default/files/snowghost_{}.jpg",
+        Webcam.BLACK_FOREST: "http://www.bigwhite.com/sites/default/files/blackforest_{}.jpg",
+        Webcam.GEM_LAKE_TOP: "http://www.bigwhite.com/sites/default/files/gemlake_{}.jpg",
+        Webcam.GEM_LAKE_BOTTOM: "http://www.bigwhite.com/sites/default/files/westridge_{}.jpg",
     }
     _webcam_indices: dict[Webcam, int] = {w: 0 for w in Webcam}
 
-    def __init__(self, webcam: Webcam, index: int | None = None):
+    def __init__(
+        self, webcam: Webcam, index: int | None = None, timeout: float | None = None
+    ):
         self._webcam = webcam
+        self._timeout = timeout
+        if timeout is None:
+            self._timeout = _DEFAULT_TIMEOUT_SECS
         if index is None:
-            BigWhite._scrape_indices()
+            BigWhite._scrape_indices(self._timeout)
             self.index = BigWhite._webcam_indices[self._webcam]
         else:
             self.index = index
 
     @staticmethod
-    def _scrape_indices():
-        response = requests.get(BigWhite._INDEX_URL)
+    def _scrape_indices(timeout=None):
+        if timeout is None:
+            timeout = _DEFAULT_TIMEOUT_SECS
+        response = requests.get(BigWhite._INDEX_URL, timeout=timeout)
         response.raise_for_status()
         for webcam in BigWhite.Webcam:
             url_format = BigWhite._URL_FORMATS[webcam]
@@ -77,11 +87,11 @@ class BigWhite:
                 raise BigWhite.BigWhiteScrapingError(
                     f"error scraping image index for webcam: {webcam.name}"
                 )
-            else:
-                BigWhite._webcam_indices[webcam] = int(image_index.group(0))
+            BigWhite._webcam_indices[webcam] = int(image_index.group(0))
 
     @property
     def formatted_url(self) -> str:
+        """The image URL for the chosen webcam's current image index."""
         return self._url_format.format(self.index)
 
     @property
@@ -90,6 +100,6 @@ class BigWhite:
 
     def read(self) -> Image:
         """fetches the raw image data for this webcam"""
-        response = requests.get(self.formatted_url)
+        response = requests.get(self.formatted_url, timeout=self._timeout)
         response.raise_for_status()
         return open_image(BytesIO(response.content))
